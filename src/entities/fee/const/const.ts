2f4b07d311f68309df08b603b2daad01fea887d1
romansmolin <romasmolin29@gmail.com>
2025-07-30 20:57:43 +0300

    Feat 7: Fixes for build

--- src/entities/fee/const/const.ts
+++ src/entities/fee/const/const.ts
@@ -1,3 +1,3 @@
 export const CURRENCIES = ['EUR', 'USD', 'GBP'] as const
-export const MAX_FEES_NUMBER = process.env.NEXT_PUBLIC_MAX_FEES_NUMBER
-export const DEFAULT_FEE = process.env.NEXT_PUBLIC_DEFAULT_FEE // 1% default fee
+export const MAX_FEES_NUMBER = Number(process.env.NEXT_PUBLIC_MAX_FEES_NUMBER)
+export const DEFAULT_FEE = Number(process.env.NEXT_PUBLIC_DEFAULT_FEE) // 1% default fee
